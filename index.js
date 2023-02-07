a584a68f4bd8ce105cf503c5af02ae87ad0643ab
Nukio <NukiXm@yandex.ru>
2023-02-07 18:04:43 +0300

    git init

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req,res) =>{
     res.send('111 Hello World!');
+});
+
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body);
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Aboba',
+    }, 'secret123');
+    res.json({
+        success:true,
+        token,
+    });
 });
 
 app.listen(4444, (error)=>{
